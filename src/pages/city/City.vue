35b08e306f81c3a7ebfbb1993f76cd847ddb89c1
Rainno <1140838639@qq.com>
2019-07-22 14:12:23 +0800

    city router

---
+++ src/pages/city/City.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="">
+     <city-header></city-header>
+    </div>
+</template>
+
+<script>
+import cityHeader from './components/Header.vue'
+export default {
+  name: 'city',
+  data () {
+    return {}
+  },
+  components: {
+    cityHeader
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -7,10 +7,12 @@
         <span class="iconfont">&#xe632;</span>
         输入城市/景点/游玩主题
       </div>
-      <div class="header-right">
-        {{this.city}}
+      <router-link to="/city">
+        <div class="header-right">
+          {{this.city}}
         <span class="iconfont arrow-icon">&#xe6aa;</span>
-      </div>
+        </div>
+      </router-link>
     </div>
 </template>
 
@@ -30,8 +32,8 @@ export default {
 @import '~styles/varibles.styl'
 .header{
   display: flex;
-  height: 0.86rem;
-  line-height:0.86rem;
+  height: $headerHeight;
+  line-height: $headerHeight;
   background-color: $bgColor;
   color:#fff;
   .header-left{
@@ -57,6 +59,7 @@ export default {
     width: 1.24rem;
     float: right;
     text-align center;
+    color: #fff;
     .arrow-icon{
       margin-left:-.04rem;
       font-size 0.24rem;
