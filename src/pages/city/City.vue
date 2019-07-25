217b65e722201e0d326ed9a7f57b03621641dc71
Rainno <1140838639@qq.com>
2019-07-25 22:03:50 +0800

    search page

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="">
       <city-header></city-header>
-      <city-search></city-search>
+      <city-search :cities="cities"></city-search>
       <city-list
         :cities="cities"
         :hotCities="hotCities"
--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -1,14 +1,77 @@
 <template>
-    <div class="search">
-      <input class="search-input" type="text" placeholder="输入城市名或拼音">
+    <div>
+      <div class="search">
+        <input
+          class="search-input"
+          type="text"
+          placeholder="输入城市名或拼音"
+          v-model="keyword"
+        >
+      </div>
+      <div
+        class="search-content"
+        ref="search"
+        v-show="keyword"
+      >
+        <ul>
+          <li
+            class="search-item border-bottom"
+            v-for="item in list"
+            :key="item"
+          >
+            {{item.name}}
+          </li>
+          <li class="search-item border-bottom" v-show="hasNoData">
+            没有找到匹配数据
+          </li>
+        </ul>
+      </div>
     </div>
 </template>
 
 <script>
+import BScroll from 'better-scroll'
 export default {
   name: 'CitySearch',
+  props: {
+    cities: Object
+  },
   data () {
-    return {}
+    return {
+      keyword: '',
+      list: [],
+      timer: null
+    }
+  },
+  mounted () {
+    this.scroll = new BScroll(this.$refs.search)
+  },
+  computed: {
+    hasNoData () {
+      return !this.list.length
+    }
+  },
+  watch: {
+    keyword () {
+      if (this.timer) {
+        clearTimeout(this.timer)
+      }
+      if (!this.keyword) {
+        this.list = []
+        return
+      }
+      this.timer = setTimeout(() => {
+        const result = []
+        for (let i in this.cities) {
+          this.cities[i].forEach((value) => {
+            if (value.spell.indexOf(this.keyword) > -1 || value.name.indexOf(this.keyword) > -1) {
+              result.push(value)
+            }
+          })
+        }
+        this.list = result
+      }, 100)
+    }
   }
 }
 </script>
@@ -28,6 +91,27 @@ export default {
     color: #666;
     box-sizing: border-box;
     padding: 0 .1rem;
+  }
+}
+.search-content {
+  position: absolute;
+  top: 1.58rem;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  z-index: 1;
+  overflow: hidden
+  background-color: #eee;
+  .search-item{
+    line-height: .62rem;
+    padding-left: .2rem;
+    color: #666;
+    background-color: #fff;
+  }
+  .border-bottom{
+      &:before {
+        border-color: #ccc;
+      }
   }
 }
 </style>
