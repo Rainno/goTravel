e0a3d7033eb2f8e8a30b634f7410e05b646291b0
Rainno <1140838639@qq.com>
2019-07-27 10:44:16 +0800

    list component

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -2,24 +2,62 @@
     <div class="">
       <detail-banner></detail-banner>
       <detail-header></detail-header>
-      <div class="content"></div>
+      <div class="content">
+        <detail-list :list="list"></detail-list>
+      </div>
     </div>
 </template>
 
 <script>
 import DetailBanner from './components/Banner.vue'
 import DetailHeader from './components/Header.vue'
+import DetailList from './components/List.vue'
+import axios from 'axios'
 export default {
   name: 'Detail',
   components: {
     DetailBanner,
-    DetailHeader
+    DetailHeader,
+    DetailList
   },
   data () {
-    return {}
+    return {
+      list: [{
+        title: '成人票',
+        children: [{
+          title: '成人三馆联票',
+          children: [{
+            title: '成人三馆联票-某一连锁店销售'
+          }]
+        }, {
+          title: '成人五馆联票'
+        }]
+      }, {
+        title: '学生票'
+      }, {
+        title: '儿童票'
+      }, {
+        title: '特价票'
+      }]
+    }
   },
-  mounted: {
-
+  mounted () {
+    this.getDetailInfo()
+  },
+  methods: {
+    getDetailInfo () {
+      axios.get('/api/detail.json')
+        .then(this.getDetailInfoSuccess)
+    },
+    getDetailInfoSuccess (res) {
+      res = res.data
+      if (res.ret && res.data) {
+        // this.swiperList = res.data.swiperList
+      // this.iconList = res.data.iconList
+      // this.recommendList = res.data.recommendList
+      // this.weekendList = res.data.weekendList
+      }
+    }
   }
 }
 </script>
--- src/pages/detail/components/Header.vue
+++ src/pages/detail/components/Header.vue
@@ -76,6 +76,7 @@ export default {
     }
   }
   .header-fixed{
+    z-index: 2;
     position: fixed;
     top: 0;
     left: 0;
